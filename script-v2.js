ce7f603398a6e819d322ef04ac968df45f9796c3
correah2009 <correah2009@gmail.com>
2018-04-23 18:05:06 -0700

    updates to locations
    can see  list of places, can show which places have websites and which don't

--- script-v2.js
+++ script-v2.js
@@ -4,43 +4,64 @@ document.addEventListener('DOMContentLoaded', function () {
   {
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAxU8S54Kn4bydbPtgg-OkDgR-d9YdQwCA&libraries=places&callback=initMap';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBcSXlSamiM6dQPrXpU86_ARlGGyNLkcnw&libraries=places&callback=initMap';
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
 
-var map;
+var map, service,markers,bounds,infoWindow;
 const azusa = {lat: 34.1167014, lng: -117.9011413};
-let infowindow;
+
 
 function initMap () {
-  infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: azusa,
     zoom: 14
   });
 
-  drawMap();
-  drawMap('cafe', google.maps.Animation.BOUNCE);
+ service = new google.maps.places.PlacesService(map);
+ infoWindow = new google.maps.InfoWindow();
+ markers = [];
 
+ drawMap()
+ .then( (results) => {
+   console.log("$$$$", results);
+   return Promise.all(
+            results.map(getWebsite)
+          );
+ })
+ .then((results) => {
+   console.log("$$$$$$$", results);
+  //  bounds = new google.maps.LatLngBounds();
+  //  results.forEach(createMarker);
+  //  map.fitBounds(bounds);
+  });
+
+ //    const placeWebsites = results.map((place) => getWebsite( place ));
+ //    console.log("$$$$$", placeWebsites);
+ //    newresult.map((place) => createMarker(place, animation));
+//      secondnewresult.map( (place) => {
+//        placeListUi(place, placeLoc, map, marker, website);
+//      }) );
+//drawMap('cafe', google.maps.Animation.BOUNCE);
 }
 
-const drawMap = (type = 'laundry', animation = null) => {
-  console.log(type, animation);
-  const service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: azusa,
-    radius: 5000,
-    type: type,
-  }, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i], animation);
+const drawMap = (type = 'cafe', animation = null) => {
+  return new Promise ((resolve, reject) => {
+    service.nearbySearch({
+      location: azusa,
+      radius: 5000,
+      type: type,
+    }, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(")))))", results);
+        resolve(results);
+      } else {
+        throw new Error('Nearby search request failed');
+        reject(status);
       }
-    } else {
-      throw new Error('Nearby search request failed');
-    }
+    });
   });
 }
 
@@ -51,18 +72,12 @@ const createMarker = (place, animation) => {
     map: map,
     position: place.geometry.location
   });
-
   marker.setAnimation(animation);
-
-  let website = '';
-  let p = new Promise((resolve, reject) => {getWebsite(place.place_id)});
-  p.then((val) => website = val);
-  callback2(place, placeLoc, map, marker, website);
 }
 
   //const place_website = getWebsite(place.place_id);
 
-const callback2 = (place, placeLoc, map, marker, website) => {
+const placeListUi = (place, placeLoc, map, marker, website) => {
   // console.log(place, placeLoc,website);
   const content = document.createElement('li');
   content.className = "list-group-item";
@@ -80,23 +95,19 @@ const callback2 = (place, placeLoc, map, marker, website) => {
   });
 }
 
-const getWebsite = (place_id) => {
-  const service = new google.maps.places.PlacesService(map);
-  // console.log(place_id);
-  service.getDetails({
-    placeId: place_id
-  }, function(place, status) {
 
-    console.log(status)
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      if (place.website) {
-        // console.log("******", place.website);
-        return place.website;
+const getWebsite = (place) => {
+  return new Promise ((resolve, reject) => {
+    service.getDetails({
+      placeId: place.place_id
+    }, (place, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log("_____", place);
+        resolve(place);
       } else {
-        return 'No website';
+        throw new Error('Get Details request failed');
+        reject(status);
       }
-    } else {
-      throw new Error('Get Details request failed');
-    }
+    });
   });
 }
